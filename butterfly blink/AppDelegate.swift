57520b9df38caff4e4f6544b89904f808f37c463
Florian Gutmann <f.gutmann@redsource.at>
2019-03-20 11:57:21 +0000

    Initial Commit

---
+++ butterfly blink/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  butterfly blink
+//
+//  Created by Florian Gutmann on 20.03.19.
+//  Copyright © 2019 Florian Gutmann. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ butterfly blink/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  butterfly blink
+//
+//  Created by Florian Gutmann on 20.03.19.
+//  Copyright © 2019 Florian Gutmann. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
